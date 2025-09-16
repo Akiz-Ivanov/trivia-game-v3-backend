c88001200ed060631529c2a96f5020d73ec0caed
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-09-16 21:58:15 +0200

    Add render.yaml for Render deployment

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ const { MONGODB_URI } = config
 
 const app = express()
 
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Your Vite dev server
+  origin: [FRONTEND_URL, 'http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }))
 
